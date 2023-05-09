48955302de9546a5efe22915684155c13e68a817
Harry Kim <56422158+ioharrykim@users.noreply.github.com>
2023-05-09 22:38:41 +0900

    Delete jigiyeok-todo directory

--- jigiyeok-todo/src/theme/theme.d.ts
+++
@@ -1,7 +0,0 @@
-import "styled-components";
-import type GlobalTheme from "./index";
-
-type GlobalThemeType = typeof GlobalTheme;
-declare module "styled-components" {
-  export interface DefaultTheme extends GlobalThemeType {}
-}
