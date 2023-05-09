1ca056f654a9ae46d4adb5dff4c5ead065e5905d
Harry Kim <56422158+ioharrykim@users.noreply.github.com>
2023-05-09 22:38:58 +0900

    Delete jigiyeok directory

--- jigiyeok/src/components/common/HelperText/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const HelperText = styled.span`
-  display: inline-block;
-  color: rgba(0, 0, 0, 0.6);
-  font-size: 12px;
-`;
-
-export default HelperText;
--- jigiyeok/src/components/common/Icon/svgr.ts
+++
@@ -1,3 +0,0 @@
-export { ReactComponent as SystemEye } from "@/assets/icons/system-eye.svg";
-export { ReactComponent as SystemHeart } from "@/assets/icons/system-heart.svg";
-export { ReactComponent as SystemAlertCircle } from "@/assets/icons/system-alert-circle.svg";
